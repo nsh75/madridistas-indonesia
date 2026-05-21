1590890dcb28e1b50ffbcd86559d281276dd3d90
rezxy99 <mrrezy11@students.amikom.ac.id>
2026-05-21 23:51:07 +0700

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -5,7 +5,7 @@ const products = [
     price: "Rp 899.000",
     category: "Jersey",
     image:
-      "https://images.unsplash.com/photo-1523398002811-999ca8dec234?q=80&w=1200&auto=format&fit=crop",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7nhnYMqkkRSucplu5CO_MyVgvhoi0XHBDXA&s,
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const products = [
     price: "Rp 199.000",
     category: "Accessories",
     image:
-      "https://images.unsplash.com/photo-1503341504253-dff4815485f1?q=80&w=1200&auto=format&fit=crop",
+      "https://i.ebayimg.com/images/g/EdYAAOSwINhj0ALu/s-l1200.jpg",
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const products = [
     price: "Rp 599.000",
     category: "Jacket",
     image:
-      "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?q=80&w=1200&auto=format&fit=crop",
+      "https://images.tokopedia.net/img/cache/700/VqbcmM/2025/4/9/1808d654-4614-4e7e-ab35-6a78ff689b7c.jpg.webp",
   },
 ];
 
