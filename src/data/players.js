ba5420e8eb6d15bc509d22afd8d246fed18b4b60
BrinoNmg <brinonamang@students.amikom.ac.id>
2026-05-21 21:53:32 +0700

    Add players data to players.js

---
+++ src/data/players.js
@@ -0,0 +1,52 @@
+const players = [
+  {
+    id: 1,
+    name: "Thibaut Courtois",
+    position: "Goalkeeper",
+    number: 1,
+    country: "Belgia",
+    image: "/images/courtois.jpg",
+  },
+  {
+    id: 2,
+    name: "Antonio Rudiger",
+    position: "Defender",
+    number: 22,
+    country: "Jerman",
+    image: "/images/rudiger.jpg",
+  },
+  {
+    id: 3,
+    name: "Jude Bellingham",
+    position: "Midfielder",
+    number: 5,
+    country: "Inggris",
+    image: "/images/bellingham.jpg",
+  },
+  {
+    id: 4,
+    name: "Federico Valverde",
+    position: "Midfielder",
+    number: 8,
+    country: "Uruguay",
+    image: "/images/valverde.jpg",
+  },
+  {
+    id: 5,
+    name: "Vinicius Junior",
+    position: "Forward",
+    number: 7,
+    country: "Brasil",
+    image: "/images/vinicius.jpg",
+  },
+  {
+    id: 6,
+    name: "Kylian Mbappe",
+    position: "Forward",
+    number: 9,
+    country: "Prancis",
+    image: "/images/mbappe.jpg",
+  },
+];
+
+export default players;
